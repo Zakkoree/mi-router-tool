3f4c5763e6841a8719597657df337ec18521fb86
Zakkoree <1223105850@qq.com>
2026-08-20 17:37:25 +0800

    1

--- install.sh
+++ install.sh
@@ -165,7 +165,7 @@ mkdir -p "$INSTALL_DIR/mikit/data" "$INSTALL_DIR/mikit/apps" "$INSTALL_DIR/mikit
 
 sed -i "s|export MIKIT_DATA_DIR=.*|export MIKIT_DATA_DIR=$APPS_DIR/.mikit_data|" "$INSTALL_DIR/mikit/core/profile"
 
-info "初始化配置完成"
+info "正在初始化配置文件..."
 CUSTOM_FILE="$INSTALL_DIR/mikit/data/custom_script.sh"
 
 chmod -R +x "$INSTALL_DIR/mikit"
@@ -173,7 +173,7 @@ chmod -R +x "$INSTALL_DIR/mikit"
 up_count=$(curl -fsSL --connect-timeout 3 -m 10 https://api.counterapi.dev/v2/zakkorees-team-5185/mikit/up -H "Authorization: Bearer ut_e8iyP5XCLm1wOuvRXqva24cmaXT6SwEcHWMbrk8P" 2>/dev/null | grep -o '"up_count":[0-9]*' | awk -F':' '{print $2}')
 
 CONFIG_FILE="$INSTALL_DIR/mikit/data/mikit_db"
-info "正在初始化配置文件..."
+
 cat <<EOF > "$CONFIG_FILE"
 config app 'mikit'
   option dnsmasq '1'
