5dbaf7099c073f3796357be81fa006e3960f1b9d
Zakkoree <1223105850@qq.com>
2026-08-21 13:29:58 +0800

    1

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -x
 clear
 C_CYAN='\033[36m'
 C_DIM='\033[2m'
@@ -172,7 +172,7 @@ main() {
 
     chmod -R +x "$INSTALL_DIR/mikit"
 
-    local up_count=$(curl -fsSLk --connect-timeout 3 -m 5 https://api.counterapi.dev/v2/zakkorees-team-5185/mikit/up -H "Authorization: Bearer ut_e8iyP5XCLm1wOuvRXqva24cmaXT6SwEcHWMbrk8P" 2>/dev/null | grep -o '"up_count":[0-9]*' | awk -F':' '{print $2}')
+    local up_count=$(curl -fsSL --connect-timeout 3 -m 10 https://api.counterapi.dev/v2/zakkorees-team-5185/mikit/up -H "Authorization: Bearer ut_e8iyP5XCLm1wOuvRXqva24cmaXT6SwEcHWMbrk8P" 2>/dev/null | grep -o '"up_count":[0-9]*' | awk -F':' '{print $2}')
 
     local CONFIG_FILE="$INSTALL_DIR/mikit/data/mikit_db"
 
